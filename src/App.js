8f09e6d9a676add035e66be3037a02bd6fbd2a44
S1rexred <165589614+S1rexred@users.noreply.github.com>
2024-07-20 04:15:56 +0400

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Items from "./components/Items"
 
-function App() {
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            items: [
+                {
+                    id: 1,
+                    title: 'Диван',
+                    img: '',
+                    desc: 'Суперский диван для всей семьи',
+                    category: 'sofa',
+                    price: '30.000'
+                },
+                {
+                    id: 2,
+                    title: 'Стул',
+                    img: '',
+                    desc: 'Суперский стул для работы за компьютером',
+                    category: 'chair',
+                    price: '4.000'
+                },
+                {
+                    id: 3,
+                    title: 'Стол',
+                    img: '',
+                    desc: 'Суперский стол для всей семьи',
+                    category: 'table',
+                    price: '10.000'
+                },
+                {
+                    id: 4,
+                    title: 'Табуретка',
+                    img: '',
+                    desc: 'Удобная табуретка с подушкой',
+                    category: 'stool',
+                    price: '1.000'
+                },
+                {
+                    id: 5,
+                    title: 'Шкаф',
+                    img: '',
+                    desc: 'Суперский шкаф для всей семьи',
+                    category: 'wordrobe',
+                    price: '20.000'
+                },
+            ]
+        }
+    }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+        <Header/>
+        <Items items={this.state.items}/>
+        <Footer/>
     </div>
-  );
+  )
+}
 }
 
-export default App;
+export default App
